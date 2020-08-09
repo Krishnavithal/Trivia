fc2ff5fdc982729c96c47a1898926e6d7de7d8d6
Krishnavithal <bsvskrishna.98@gmail.com>
2020-08-09 16:42:32 +0530

    Updates

---
+++ precache-manifest.eddb9b019ae37d4afc780640c1d10267.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1c2f0f2fcfb3a5f9869d985aa5d103",
+    "url": "/Trivia/index.html"
+  },
+  {
+    "revision": "d22229a06b638756d926",
+    "url": "/Trivia/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "0ba3aa3337ccc21c9ebb",
+    "url": "/Trivia/static/css/main.c55d19e8.chunk.css"
+  },
+  {
+    "revision": "d22229a06b638756d926",
+    "url": "/Trivia/static/js/2.c8423780.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trivia/static/js/2.c8423780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba3aa3337ccc21c9ebb",
+    "url": "/Trivia/static/js/main.c9f3f1f1.chunk.js"
+  },
+  {
+    "revision": "813061b9eea68719fc0c",
+    "url": "/Trivia/static/js/runtime-main.d1b2b455.js"
+  },
+  {
+    "revision": "1ecd204c0ade07ed5213fcac84da45f3",
+    "url": "/Trivia/static/media/background.1ecd204c.jpg"
+  },
+  {
+    "revision": "dc36a31da9d289f9e4d33ec2bb04d4ac",
+    "url": "/Trivia/static/media/background1.dc36a31d.jpg"
+  }
+]);
